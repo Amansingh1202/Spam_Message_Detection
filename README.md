101dbec1a05c3c6baa47bcdab70d468c30228079
Amansingh1202 <aks001235@gmail.com>
2021-05-04 11:08:45 +0530

    added demo

---
+++ README.md
@@ -0,0 +1,5 @@
+### This app classifies spam messages given as an input from the user.The classification is done by a ML model using Naive Bayes Algorithm.
+
+## Demo
+All the entered messages are classified as Spam(denoted by red) or non-spam(denoted by green).
+![Demo1](demo/demo1.PNG)
--- client/app.py
+++ client/app.py
@@ -1,20 +1,33 @@
-from flask import Flask,jsonify,request,render_template
+from flask import Flask, jsonify, request, render_template
+
 app = Flask(__name__)
 from sklearn.feature_extraction.text import CountVectorizer
 import pickle
 
-@app.route('/checkSpam',methods = ['GET','POST'])
-def checkSpam():
-	test = []
-	text = request.form.get('data')
-	test.append(text)
-	with open('../models/spam_classifier.pkl','rb') as f:
-		loaded_clf = pickle.load(f)
-	with open('../models/text_vectorizer.pkl','rb') as f1:
-		text_vectorizer1 = pickle.load(f1)  
-	test_vector = text_vectorizer1.transform(test)
-	return render_template("response.html",decision = loaded_clf.predict(test_vector)[0])
+all_spam = []
 
-@app.route('/',methods = ['GET'])
+
+@app.route("/checkSpam", methods=["GET", "POST"])
+def checkSpam():
+    test = []
+    text = request.form.get("data")
+    test.append(text)
+    with open("../models/spam_classifier.pkl", "rb") as f:
+        loaded_clf = pickle.load(f)
+    with open("../models/text_vectorizer.pkl", "rb") as f1:
+        text_vectorizer1 = pickle.load(f1)
+    test_vector = text_vectorizer1.transform(test)
+    decision = loaded_clf.predict(test_vector)[0]
+    new_spam = {"decision": decision, "text_value": text}
+    all_spam.append(new_spam)
+    print(all_spam)
+    return render_template("index.html", all_spam=all_spam)
+
+
+@app.route("/", methods=["GET"])
 def index():
-	return render_template("index.html")
+    return render_template("index.html", all_spam=all_spam)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
